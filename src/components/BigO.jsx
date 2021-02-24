1f44f59794b8ac0b3321f5acb06bd4935f123433
bg-write <bradywgerber@gmail.com>
2021-02-24 10:15:11 -0500

    Update README

---
+++ src/components/BigO.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BigO() {
+	return (
+		<div>
+			<h1>Big O</h1>
+		</div>
+	);
+}
+
+export default BigO;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact() {
+	return (
+		<div>
+			<h1>CONTACT</h1>
+		</div>
+	);
+}
+
+export default Contact;
