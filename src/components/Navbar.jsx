01f30eb5afa5260f011462a6a6bda3d3b97d2b45
bg-write <bradywgerber@gmail.com>
2021-02-25 10:59:01 -0500

    Add initial Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,27 +4,31 @@ import { Link } from 'react-router-dom';
 function Navbar() {
 	return (
 		<div>
-			<img src="https://i.imgur.com/22Hi6M0.jpg" alt="homer" width="50%" />
-			<ul>
-				<li>
+			<img
+				src="https://i.imgur.com/22Hi6M0.jpg"
+				alt="homer"
+				class="img-fluid"
+			/>
+			<ul class="nav">
+				<li class="nav-item">
 					<Link to="/">Home</Link>
 				</li>
-				<li>
+				<li class="nav-item">
 					<Link to="/about">About</Link>
 				</li>
-				<li>
+				<li class="nav-item">
 					<Link to="/contact">Contact</Link>
 				</li>
-				<li>
+				<li class="nav-item">
 					<Link to="/big-o">Big O</Link>
 				</li>
-				<li>
+				<li class="nav-item">
 					<Link to="/algorithms">Algorithms</Link>
 				</li>
-				<li>
+				<li class="nav-item">
 					<Link to="/data-structures">Data Structures</Link>
 				</li>
-				<li>
+				<li class="nav-item">
 					<Link to="/ttt">Stressed?</Link>
 				</li>
 			</ul>
