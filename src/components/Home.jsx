d82e924bd547ae6cb274618b175f8664f40613db
bg-write <bradywgerber@gmail.com>
2021-03-12 13:33:48 -0500

    CSS spinning donut

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,13 @@ function Home() {
 			<main>
 				<h1>BIG D'OH: HOMER READS 'CRACKING THE CODING INTERVIEW'</h1>
 				<hr />
+				<img
+					id="donut"
+					class="loader"
+					alt="donut"
+					src="https://i.imgur.com/7NqMseE.jpg"
+					width="100px"
+				/>
 				<p>
 					If Homer can do it, so can you. No donuts were harmed in the making of
 					this React app.
