37b8ce84c793992e14088258eef1322c665e4a99
bg-write <bradywgerber@gmail.com>
2021-04-23 09:52:51 -0400

    Back from vacation, add Big O YouTube links

--- src/components/BigO/BigO.jsx
+++ src/components/BigO/BigO.jsx
@@ -20,8 +20,10 @@ function BigO() {
 				<p>
 					<i>
 						<b>Big O Notation</b> - How efficient is your algorithm. Based on
-						run time (how many steps do you have to take?) and space (how much
-						memory do you need?).
+						run time (how many steps do you take?) and space (how much memory do
+						you need?). (<Link to="https://www.youtube.com/watch?v=__vX2sjlpXU">
+							YouTube
+						</Link>)
 					</i>
 				</p>
 				<a href="https://www.bigocheatsheet.com/">
@@ -35,54 +37,64 @@ function BigO() {
 
 				<h2>O(1) - Constant</h2>
 				<p>
-					When the input size doesn't matter; Whether your input (N) is 1 or
-					1,000 values long, the algorithm still just takes one "step."
+					The input size doesn't matter. Whether your input (N) is 1 or 1,000
+					values long, the algorithm still just takes one "step." (
+					<Link to="https://youtu.be/__vX2sjlpXU?t=114">YouTube</Link>)
 				</p>
 				<hr />
 
 				<h2>O(log N) - Logarithmic</h2>
 				<p>
-					Cut the number of steps each time in half. EX Binary Search; look up a
-					name in the phone book, read a name, and flip forwards or backwards
-					depending on how close you are.
+					Cut the number of steps each time in half; if N is doubled, you only
+					have to do 1 more operation. EX Binary Search: look up a name in the
+					phone book, read a name, and flip forwards or backwards depending on
+					how close you are. (
+					<Link to="https://www.youtube.com/watch?v=4mqtiLjtddY">YouTube</Link>)
 				</p>
 				<hr />
 
 				<h2>O(N) - Linear</h2>
 				<p>
-					When N is the length of your input; if your list has 10 items, you
-					print 10 times. If you have 1,000 items, you print 1,000 times. EX the
-					basic "For Loop" (for each item, do this specific thing).
+					N is the length of your input. If your list has 10 items, you print 10
+					times. EX A basic "For Loop": for each item, do a specific thing. (
+					<Link to="https://youtu.be/__vX2sjlpXU?t=157">YouTube</Link>)
 				</p>
 				<hr />
 
 				<h2>O(N log N) - Quasilinear</h2>
 				<p>
 					For each time N grows, the time required grows linearly AND
-					logarithmically. EX Quick Sort and Merge Sort.
+					logarithmically. EX A basic "While For Loop": While something is true,
+					for each item, do this specific thing. Also, Quick Sort and Merge
+					Sort. (
+					<Link to="https://www.youtube.com/watch?v=K3NluEdHkao">YouTube</Link>)
 				</p>
 				<hr />
 
 				<h2>O(N^2) - Quadratic</h2>
 				<p>
-					For each time N grows, the processing time doubles; if your list has
-					10 items, you have to print 100 times. EX nesting two loops in the
-					same function.
+					For each time N grows, the processing time doubles. If your list has
+					10 items, you have to print 100 times. EX Nesting two loops in the
+					same function. (
+					<Link to="https://youtu.be/__vX2sjlpXU?t=203">YouTube</Link>)
 				</p>
 				<hr />
 
 				<h2>O(2^N) - Exponential</h2>
 				<p>
-					Doubles with every additional input; if N = 2, your algorithm runs 4
-					times, and if N = 3, your algorithm runs 8 times. EX Fibonacci.
+					Doubles with every additional input. If N = 2, your algorithm runs 4
+					times, and if N = 3, your algorithm runs 8 times. EX Recursion and
+					Fibonacci. (
+					<Link to="https://www.youtube.com/watch?v=5AxcXHPB3GE">YouTube</Link>)
 				</p>
 				<hr />
 
 				<h2>O(N!) - Factorial</h2>
 				<p>
-					When every possible solution may have to be calculated in order to
-					determine the correct output. 6! = 6*5*4*3*2*1 = 720. EX Brute Force,
-					the Traveling Salesman problem.
+					When you have to calculate every possible solution to determine the
+					correct output. 6! = 6*5*4*3*2*1 = 720. EX Brute Force and the
+					Traveling Salesman problem. (
+					<Link to="https://youtu.be/5AxcXHPB3GE?t=79">YouTube</Link>)
 				</p>
 				<hr />
 			</main>
