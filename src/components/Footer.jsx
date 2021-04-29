42b91563367ca9a5e223f7969deb0934d8365529
bg-write <bradywgerber@gmail.com>
2021-04-29 12:18:42 -0400

    Update Footer with local reps link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,16 @@ function Footer() {
 		<>
 			<footer>
 				<p>
-					<small>Copyright &copy; Big D'oh 2021</small>
+					<small>
+						Copyright &copy; Big D'oh 2021.{' '}
+						<a
+							href="https://www.house.gov/representatives/find-your-representative"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Find Your Local Representatives.
+						</a>
+					</small>
 				</p>
 			</footer>
 		</>
