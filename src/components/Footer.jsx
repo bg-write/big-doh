9abeb8fa3af92ea67d068e4d6864a3d2f1cb03c4
bg-write <bradywgerber@gmail.com>
2021-05-05 10:46:19 -0400

    Footer, clean up placeholder links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,22 @@ function Footer() {
 				<small>
 					<p>
 						<div>
+							<a href="/">Home</a>
+						</div>
+						<div>
+							<a href="/big-o">Big O</a>
+						</div>
+						<div>
+							<a href="/algorithms">Algorithms</a>
+						</div>
+						<div>
+							<a href="/data-structures">Data Structures</a>
+						</div>
+						<div>
+							<a href="/ttt">Study Break</a>
+						</div>
+						{/* ICEBOX: Make React Link work to move window view to top of page from footer */}
+						{/* <div>
 							<Link to="/">Home</Link>
 						</div>
 						<div>
@@ -21,7 +37,7 @@ function Footer() {
 						</div>
 						<div>
 							<Link to="/ttt">Study Break</Link>
-						</div>
+						</div> */}
 					</p>
 
 					<p>
