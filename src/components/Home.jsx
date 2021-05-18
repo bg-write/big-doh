00682a54a141facda8aa12836464e6051fd4f58f
bg-write <bradywgerber@gmail.com>
2021-05-18 09:07:22 -0400

    CSS update donut home container flex

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,16 @@ function Home() {
 			<main>
 				<div id="flex-container-home">
 					<h1 id="homerText">
-						IF HOMER<br></br>CAN DO IT,<br></br>YOU CAN<br></br>TOO.
+						IF HOMER <br></br>CAN DO IT, <br></br>YOU CAN <br></br>TOO.
 					</h1>
-					<img
-						id="donut-home"
-						className="donut loader"
-						alt="donut"
-						src="https://i.imgur.com/ske1xNn.png"
-					/>
+					<div id="donut-home-container" className="center-text">
+						<img
+							id="donut-home"
+							className="donut loader"
+							alt="donut"
+							src="https://i.imgur.com/ske1xNn.png"
+						/>
+					</div>
 				</div>
 
 				{/* ICEBOX: Move marquee into its own component */}
@@ -53,7 +55,9 @@ function Home() {
 
 				<div id="home-intro" className="center-text">
 					<p className="border-text">
-						<b>BIG O?<br></br>MORE LIKE BIG <i>D'OH</i>!</b>
+						<b>
+							BIG O?<br></br>MORE LIKE BIG <i>D'OH</i>!
+						</b>
 					</p>
 					<p>
 						Read 'Cracking the Coding Interview' but still not getting Big O?
