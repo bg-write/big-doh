64514bbe6c8a945555d8f77e9709943e14115d64
bg-write <bradywgerber@gmail.com>
2021-05-24 09:19:33 -0400

    Simplify Footer, update Icebox folder with original Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,232 +4,54 @@ function Footer() {
 	return (
 		<>
 			<footer id="footer-container">
-				<div id="footer-grid">
-					<div id="footer-internal-links">
-						<ul>
-							<li>
-								<a href="/">Home</a>
-							</li>
-							<li>
-								<a href="/big-o">Big O</a>
-							</li>
-							<li>
-								<a href="/algorithms">Algorithms</a>
-							</li>
-							<li>
-								<a href="/data-structures">Data Structures</a>
-							</li>
-							<li>
-								<a href="/ttt">Study Break</a>
-							</li>
-						</ul>
-					</div>
-
-					<div id="blank"></div>
-					<div id="footer-credit-brady-links">
-						<i>Built by</i>
-						<br></br>BRADY GERBER
-						<div className="remove-underline">
-							<a
-								href="https://github.com/bg-write"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="symbol-font"
-							>
-								GITHUB
-							</a>{' '}
-							<a
-								href="https://www.linkedin.com/in/brady-gerber/"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="symbol-font"
-							>
-								LINKEDIN
-							</a>{' '}
-							<a
-								href="https://twitter.com/BradyWGerber"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="symbol-font"
-							>
-								TWITTER
-							</a>
-						</div>
-						<a
-							href="https://bradygerber.com/"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Website
-						</a>
-						<br></br>Gmail (
-						<a
-							href="mailto:bradywgerber@gmail.com"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							bradywgerber
-						</a>
-						)
-					</div>
-					<div id="blank"></div>
-					<div id="blank"></div>
-					<div id="footer-credit-molly-links">
-						<i>Designed by</i>
-						<br></br>MOLLY HODSON
-						<div className="remove-underline">
-							<a
-								href="https://dribbble.com/mollyhodson"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="symbol-font"
-							>
-								DRIBBBLE
-							</a>{' '}
-							<a
-								href="https://www.linkedin.com/in/molly-hodson/"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="symbol-font"
-							>
-								LINKEDIN
-							</a>{' '}
-							<a
-								href="https://twitter.com/MollyHodson"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="symbol-font"
-							>
-								TWITTER
-							</a>
-						</div>
-						<a
-							href="https://www.mollyhodson.online/"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Website
-						</a>
-						<br></br>Gmail (
-						<a
-							href="mailto:mollyphodson@gmail.com"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							mollyhodsononline
-						</a>
-						)
-					</div>
-					<div id="footer-external-links">
-						<p>
-							All photos and gifs are taken from General Assembly except when
-							linked to their original sources.
-						</p>
-						<div>
-							Other helpful links:
+				<small>
+					<div id="footer-grid">
+						<div id="footer-internal-links">
 							<ul>
 								<li>
-									<a
-										href="https://www.crackingthecodinginterview.com/"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										Cracking the Coding Interview
-									</a>
+									<a href="/">Home</a>
 								</li>
 								<li>
-									<a
-										href="https://eloquentjavascript.net/"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										Eloquent JavaScript
-									</a>
+									<a href="/big-o">Big O</a>
 								</li>
 								<li>
-									<a
-										href="https://css-tricks.com/almanac/"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										CSS-Tricks
-									</a>
+									<a href="/algorithms">Algorithms</a>
 								</li>
 								<li>
-									<a
-										href="https://flexboxfroggy.com/"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										Flexbox Froggy
-									</a>
+									<a href="/data-structures">Data Structures</a>
 								</li>
 								<li>
-									<a
-										href="https://cssgridgarden.com/"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										Grid Garden
-									</a>
-								</li>
-								<li>
-									<a
-										href="https://www.geeksforgeeks.org/"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										GeeksforGeeks
-									</a>
-								</li>
-								<li>
-									<a
-										href="https://www.freecodecamp.org/news/big-o-notation/"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										freeCodeCamp
-									</a>
-								</li>
-								<li>
-									<a
-										href="https://www.youtube.com/c/WebDevSimplified"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										Web Dev Simplified
-									</a>
-								</li>
-								<li>
-									<a
-										href="https://nickbulljs.com/"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										Nick Bull's 3-2-1 Newsletter
-									</a>
-								</li>
-								<li>
-									<a
-										href="https://www.youtube.com/watch?v=sVZX0XvEBhk"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										Coding Journey
-									</a>
-								</li>
-								<li>
-									<a
-										href="https://leetcode.com/"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										LeetCode
-									</a>
+									<a href="/ttt">Study Break</a>
 								</li>
 							</ul>
 						</div>
-						<small>
+						<ul>
+							<li>
+								Built by{' '}
+								<a
+									href="https://bradygerber.com/"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Brady Gerber
+								</a>
+							</li>
+							<li>
+								Designed by{' '}
+								<a
+									href="https://www.mollyhodson.online/"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Molly Hodson
+								</a>
+							</li>
+						</ul>
+						<div id="footer-external-links">
+							<p>
+								All photos and gifs are taken from General Assembly <br></br>
+								except when linked to their original sources.
+							</p>
 							<p>Copyright &copy; Big D'oh 2021.</p>
 							<p>
 								{' '}
@@ -241,9 +63,9 @@ function Footer() {
 									Find Your Local Representatives.
 								</a>
 							</p>
-						</small>
+						</div>
 					</div>
-				</div>
+				</small>
 			</footer>
 		</>
 	);
---
+++ src/components/Icebox/Icebox.jsx
@@ -0,0 +1,248 @@
+// FOR ALL THE THINGS WE MUST USE LATER, WANT TO USE LATER
+
+//FOOTER
+// All the links we don't have room for but might want to still place somewhere?
+/*
+<footer id="footer-container">
+				<div id="footer-grid">
+					<div id="footer-internal-links">
+						<ul>
+							<li>
+								<a href="/">Home</a>
+							</li>
+							<li>
+								<a href="/big-o">Big O</a>
+							</li>
+							<li>
+								<a href="/algorithms">Algorithms</a>
+							</li>
+							<li>
+								<a href="/data-structures">Data Structures</a>
+							</li>
+							<li>
+								<a href="/ttt">Study Break</a>
+							</li>
+						</ul>
+					</div>
+
+					<div id="blank"></div>
+					<div id="footer-credit-brady-links">
+						<i>Built by</i>
+						<br></br>BRADY GERBER
+						<div className="remove-underline">
+							<a
+								href="https://github.com/bg-write"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="symbol-font"
+							>
+								GITHUB
+							</a>{' '}
+							<a
+								href="https://www.linkedin.com/in/brady-gerber/"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="symbol-font"
+							>
+								LINKEDIN
+							</a>{' '}
+							<a
+								href="https://twitter.com/BradyWGerber"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="symbol-font"
+							>
+								TWITTER
+							</a>
+						</div>
+						<a
+							href="https://bradygerber.com/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Website
+						</a>
+						<br></br>Gmail (
+						<a
+							href="mailto:bradywgerber@gmail.com"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							bradywgerber
+						</a>
+						)
+					</div>
+					<div id="blank"></div>
+					<div id="blank"></div>
+					<div id="footer-credit-molly-links">
+						<i>Designed by</i>
+						<br></br>MOLLY HODSON
+						<div className="remove-underline">
+							<a
+								href="https://dribbble.com/mollyhodson"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="symbol-font"
+							>
+								DRIBBBLE
+							</a>{' '}
+							<a
+								href="https://www.linkedin.com/in/molly-hodson/"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="symbol-font"
+							>
+								LINKEDIN
+							</a>{' '}
+							<a
+								href="https://twitter.com/MollyHodson"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="symbol-font"
+							>
+								TWITTER
+							</a>
+						</div>
+						<a
+							href="https://www.mollyhodson.online/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Website
+						</a>
+						<br></br>Gmail (
+						<a
+							href="mailto:mollyphodson@gmail.com"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							mollyhodsononline
+						</a>
+						)
+					</div>
+					<div id="footer-external-links">
+						<p>
+							All photos and gifs are taken from General Assembly except when
+							linked to their original sources.
+						</p>
+						<div>
+							Other helpful links:
+							<ul>
+								<li>
+									<a
+										href="https://www.crackingthecodinginterview.com/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Cracking the Coding Interview
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://eloquentjavascript.net/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Eloquent JavaScript
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://css-tricks.com/almanac/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										CSS-Tricks
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://flexboxfroggy.com/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Flexbox Froggy
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://cssgridgarden.com/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Grid Garden
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://www.geeksforgeeks.org/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										GeeksforGeeks
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://www.freecodecamp.org/news/big-o-notation/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										freeCodeCamp
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://www.youtube.com/c/WebDevSimplified"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Web Dev Simplified
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://nickbulljs.com/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Nick Bull's 3-2-1 Newsletter
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://www.youtube.com/watch?v=sVZX0XvEBhk"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Coding Journey
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://leetcode.com/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										LeetCode
+									</a>
+								</li>
+							</ul>
+						</div>
+						<small>
+							<p>Copyright &copy; Big D'oh 2021.</p>
+							<p>
+								{' '}
+								<a
+									href="https://www.house.gov/representatives/find-your-representative"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Find Your Local Representatives.
+								</a>
+							</p>
+						</small>
+					</div>
+				</div>
+			</footer>
+*/
