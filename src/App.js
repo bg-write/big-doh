8614395c65efaadcb5b73101ca34476b53cf6cae
bg-write <bradywgerber@gmail.com>
2021-05-26 08:38:51 -0400

    Pre Loadable Components installation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import BigO from './components/BigO/BigO';
@@ -7,18 +8,17 @@ import DataStructures from './components/DataStructures/DataStructures';
 import TTT from './components/TTT/TTT';
 import Footer from './components/Footer';
 import NavBar from './components/Navbar';
-import { Route, Link } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App grid">
-			<NavBar />
-			<Route exact path="/" component={Home} />
-			<Route exact path="/big-o" component={BigO} />
-			<Route exact path="/algorithms" component={Algorithms} />
-			<Route exact path="/data-structures" component={DataStructures} />
-			<Route exact path="/ttt" component={TTT} />
-			<Footer />
+						<NavBar />
+						<Route exact path="/" component={Home} />
+						<Route exact path="/big-o" component={BigO} />
+						<Route exact path="/algorithms" component={Algorithms} />
+						<Route exact path="/data-structures" component={DataStructures} />
+						<Route exact path="/ttt" component={TTT} />
+						<Footer />
 		</div>
 	);
 }
