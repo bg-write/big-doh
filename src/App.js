e12a414d1c28ff2458d4a045aaec3e1c3255237e
bg-write <bradywgerber@gmail.com>
2021-05-26 09:05:02 -0400

    Update Home route with React.lazy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
+
 import BigO from './components/BigO/BigO';
 import Algorithms from './components/Algorithms/Algorithms';
 import DataStructures from './components/DataStructures/DataStructures';
 import TTT from './components/TTT/TTT';
 import Footer from './components/Footer';
 import NavBar from './components/Navbar';
+const Home = lazy(() => import('./components/Home'));
 
 function App() {
 	return (
-		<div className="App grid">
+		<Router>
+			<Suspense fallback={<div>Loading...</div>}>
+					<div className="App grid">
 						<NavBar />
 						<Route exact path="/" component={Home} />
 						<Route exact path="/big-o" component={BigO} />
@@ -19,7 +22,9 @@ function App() {
 						<Route exact path="/data-structures" component={DataStructures} />
 						<Route exact path="/ttt" component={TTT} />
 						<Footer />
-		</div>
+					</div>
+			</Suspense>
+		</Router>
 	);
 }
 
